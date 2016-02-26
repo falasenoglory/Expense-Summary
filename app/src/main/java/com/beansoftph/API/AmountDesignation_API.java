d5671d65f152a7b5d0b4155100c8f2c59c367e47
chanferolino <chanferolino@gmail.com>
2016-02-26 09:45:36 +0800

    added httpUtils                                                                                                                         create api

--- app/src/main/java/com/beansoftph/API/AmountDesignation_API.java
+++ app/src/main/java/com/beansoftph/API/AmountDesignation_API.java
@@ -1,7 +1,57 @@
 package com.beansoftph.API;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.beansoftph.utils.HttpUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 /**
  * Created by Christian on 2/26/2016.
  */
 public class AmountDesignation_API {
+
+    public static final String API_URL = "http://expensesummary.webraw.net/db_api/amount_designations";
+
+    public static ArrayList<String> getAmountDesignations(String sring, @NonNull String requestMethod) {
+        String json = HttpUtils.getResponse(API_URL, requestMethod);
+
+        if (TextUtils.isEmpty(json)) {
+            Log.d("Hohoho", "wala");
+
+            return null;
+        }
+
+        // Here we will now parse the json response and convert it into a Weather object.
+        String name;
+        ArrayList<String> amountDesignation;
+        amountDesignation = new ArrayList<>();
+
+        try {
+
+
+
+            JSONArray jArray = new JSONArray(json);
+            JSONObject w0 = jArray.getJSONObject(50);
+
+            Log.d("Hohoho", jArray.toString());
+            for (int i=0;i<jArray.length();i++){
+                name = w0.getString("name").toUpperCase();
+                amountDesignation.add(name);
+
+            }
+            return amountDesignation;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
